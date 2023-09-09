86c491dc0c44bce72b94587a44c195d2e80e2879
Douglas Marques <douglasmarques37@gmail.com>
2023-09-09 19:37:09 -0300

    feat: configurando swagger no server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from "express"
 import mongoose from "mongoose"
 import Cors from "cors"
+
+import swaggerUI from "swagger-ui-express"
+import swaggerjsDoc from "swagger-jsdoc"
+
 import Cards from "./dbCards.js"
+
 import "dotenv/config"
 
 // Variaveis de Ambiente
@@ -12,6 +17,23 @@ const porta = process.env.PORT
 const app = express()
 const port = porta || 8001
 const connection_url = url
+
+// swagger
+const swaggerAPIDesc = swaggerjsDoc({
+    swaggerDefinition: {
+        info: {
+            title: "API dating",
+            cersion: "1.0.0"
+        }
+    },
+    apis: ["server.js"]
+})
+
+const options = {
+    customCss: '.swagger-ui .topbar { display: none }'
+}
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerAPIDesc, options))
 
 // Middleware
 app.use(express.json())
@@ -24,8 +46,33 @@ mongoose.connect(connection_url, {
 })
 
 // API Endpoints
-app.get("/", (req, res) => res.status(200).send("Hello Node"))
+app.get("/", (req, res) => res.status(200).send("Bem-vindo a API dating"))
 
+/**
+* @swagger
+* /dating/cards:
+*   post:
+*     description: Criando Cards
+*     requestBody:
+*       content: 
+*         application/json
+*     responses:
+*       201:
+*         description: Card Cadastrado!
+*       500:
+*         description: Card Não Cadastrado!
+*     parameters:
+*       - name: name
+*         in: application/json
+*         required: true
+*         type: string
+*         description: nome do card
+*       - name: imgUrl
+*         in: application/json
+*         required: true
+*         type: string
+*         description: url da imagem
+*/
 app.post("/dating/cards", async (req, res) => {
     const dbCard = req.body
 
@@ -37,6 +84,17 @@ app.post("/dating/cards", async (req, res) => {
     }
 })
 
+/**
+* @swagger
+* /dating/cards:
+*   get:
+*     description: Listando os Cards
+*     responses: 
+*       200:
+*         description: Sucesso
+*       500:
+*         description: Servidor Não Encontrado
+*/
 app.get("/dating/cards", async (req, res) => {
     try {
         const data = await Cards.find()
